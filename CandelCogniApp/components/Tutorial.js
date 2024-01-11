fc5974ac526ac048a4f9df501a23290057008f01
AlePatata <alejandra.campos.u@ug.uchile.cl>
2024-01-10 21:31:41 -0300

    Tutorial card feedback and Documentation

--- CandelCogniApp/components/Tutorial.js
+++ CandelCogniApp/components/Tutorial.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect, useRef} from 'react';
 import {View, Text, TouchableOpacity, Animated, Easing, Modal, Image} from 'react-native';
 import CustomButton from './buttons/button'
 import pattern from './images/pattern';
-import DisplayAnImage from './images/DisplayAnImage';
 import globalStyles from '../styles/globalStyles';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import {
@@ -16,66 +15,58 @@ const withoutImage = {id:1,"path":require("./../assets/target.png"), "name":"PNG
 /** The tutorial is a simpliest version of WithImages, there is always 3 cards and the same level.
  * The mechanism of ask for an image is actionate by a button.
  * 
- * @param {*} param0 
+ * @param {*}  
  * @returns 
  */
 const Tutorial = ({ navigation }) => {
+    const [cards, setCards] = useState([]); // cards is a list of the 3 cards in turn to show.
+    const [qcards, setQCards] = useState([withoutImage, withoutImage, withoutImage]); // qcards is a list of the 3 cards in turn.
+    const [targetImage, setTargetImage] = useState(withoutImage); // targetImage is the image to ask.
+    const [score, setScore] = useState(0); // is the record of the correct score of the game.
+    const [errors, setErrors] = useState(0); // is the record of the incorrect score of the game.
+    const [feedbackMessage, setFeedbackMessage] = useState(''); // is the message to show after pick a card.
+    const [pause, setPause] = useState(false); // pause is the element to stop the game and create a feedback instance.
 
-    const [cards, setCards] = useState([]);
-    const [qcards, setQCards] = useState([withoutImage, withoutImage, withoutImage]);
-    const [targetImage, setTargetImage] = useState(withoutImage);
-    const [score, setScore] = useState(0);
-    const [errors, setErrors] = useState(0);
-    const [feedbackMessage, setFeedbackMessage] = useState('');
-    const [pause, setPause] = useState(false);
+    var level = 9; // corresponds to the "fruit level" designated in pattern.js.
+    const images = pattern.find(item => item[0] === level )[1]; // list of elements containing image addresses.
 
-    var level = 9;
-    const images = pattern.find(item => item[0] === level )[1];
+    const animatedValue = useRef(new Animated.Value(0)).current; // animated object for linear movements.
+    const rotationValue = useRef(new Animated.Value(0)).current; // animated object for card movements.
 
-    const animatedValue = useRef(new Animated.Value(0)).current;
-    const rotationValue = useRef(new Animated.Value(0)).current;
+    const [userReady, setUserReady] = useState(false); // element that allows you to turn over the cards.
+    const [showInstructions, setShowInstructions] = useState(true); // element for show the instruction modal.
 
-    const [userReady, setUserReady] = useState(false);
-    const [showInstructions, setShowInstructions] = useState(true);
-
-
+    /** Configuration for linear movement animation */
     useEffect(() => {
-        // Configurar la animación
         Animated.timing(animatedValue, {
           toValue: 1,
-          duration: 1000, // duración en milisegundos
-          easing: Easing.linear, // tipo de interpolación
-          useNativeDriver: true, // mejora el rendimiento
+          duration: 1000, 
+          easing: Easing.linear, 
+          useNativeDriver: true, 
         }).start();
     }, []);
 
-
+    /** Game logic */
     useEffect(() => {
         let randomimages = [];
-        do {
+        do { // Make a random array of 3 images
             randomimages = Array.from(
             { length: 3 },
             () => images[Math.floor(Math.random() * images.length)],
             );
-        } while (
+        } while ( // As long as they are all different
             randomimages.some(
                 (image, index) =>
                   randomimages.slice(index + 1).some((otherImage) => image.id === otherImage.id)
             )
         );
-        Animated.timing(rotationValue, {
-            toValue: 1,
-            duration: 500,
-            easing: Easing.linear,
-            useNativeDriver: true,
-          }).start(() => {
-            setCards(randomimages);
-            setQCards(randomimages);
-            setTargetImage(
-                randomimages[Math.floor(Math.random() * randomimages.length)],
-            );
-            rotationValue.setValue(0);
-        });
+
+        // Save the random cards in both cards and qcards:
+        setCards(randomimages);
+        setQCards(randomimages);
+        setTargetImage( // Pick a random card from the selection
+            randomimages[Math.floor(Math.random() * randomimages.length)],
+        );
         
         console.log(
             '\n----------------randomimages--------------\n ',
@@ -84,8 +75,9 @@ const Tutorial = ({ navigation }) => {
         );
         setUserReady(false);
     }, 
-    [score, errors]); // Dependencias de puntuación y errores para iniciar nuevos niveles
+    [score, errors]); // Score dependencies and errors to start new levels
         
+    /** Mechanics to distinguish the correct card after choosing it */
     const handleCardClick = clickedIndex => {
 
         console.log(
@@ -98,48 +90,39 @@ const Tutorial = ({ navigation }) => {
             targetImage,
             '\n----------------------------------\n',
         );
-        setCards(qcards);
-        setPause(true);
-        // Verificar si el índice seleccionado es correcto
+        setCards(qcards); // Show the asked cards again
+        setPause(true); // Instance for feedback
+        // Check if the selected index is correct
         if (qcards[clickedIndex].id === targetImage.id) {
             setTimeout(()=> {setScore(score + 1); setPause(false) },3000)
-           
             setFeedbackMessage('¡Correcto!');
         } else {
             setTimeout(()=> {setScore(score + 1); setPause(false) },3000)
-            
             setFeedbackMessage('¡Incorrecto!');
         }   
     };
-        
+    
+    /** Starts a new round */
     const startNewLevel = () => {
-        Animated.timing(rotationValue, {
-            toValue: 1,
-            duration: 500,
-            easing: Easing.linear,
-            useNativeDriver: true,
-        }).start(() => {
-            setCards([withoutImage, withoutImage, withoutImage]); // Tarjetas sin iconos
-            rotationValue.setValue(0);
-        });    
+        setCards([withoutImage, withoutImage, withoutImage]); // Reset the cards
         setUserReady(true);
     };
-    const rotateCard = rotationValue.interpolate({
+    const rotateCard = rotationValue.interpolate({ // Flip a card animation
         inputRange: [0, 1],
         outputRange: ['0deg', '180deg'],
     });
 
     return (
-
-        <View style={globalStyles.whitecontainer} >
+        <View style={globalStyles.whitecontainer} > 
+            {/*X and ? Buttons*/}
             <TouchableOpacity onPress={() => navigation.navigate('MainPage')} style={[globalStyles.supder, {borderWidth: 2, borderRadius: 18}]}>
                 <FontAwesomeIcon icon={faXmark} size={20} color={colors.black}/>
             </TouchableOpacity>
-            
             <TouchableOpacity onPress={() => setShowInstructions(true)} style={globalStyles.supizq}>
                 <FontAwesomeIcon icon={faCircleQuestion} size={20} color={colors.black}/>
             </TouchableOpacity>
             
+            {/* Instruction Modal */}
             {showInstructions && (
                 <Modal
                 animationType="slide"
@@ -159,58 +142,76 @@ const Tutorial = ({ navigation }) => {
                 </Modal>
                 )
             }
+
+            {/* 3 Cards */}
             <View style={{flexDirection: 'row', alignItems: 'center', marginBottom: 20}}>
                 {cards.map((imagen, index) => (
                     <TouchableOpacity
                         key={index}
                         style={globalStyles.card}
-                        onPress={() => { if(userReady && !pause) handleCardClick(index)}}>
-                        {imagen && <Image
-                            style={[
-                            globalStyles.tinyLogo,
-                            globalStyles.card,
-                            //{borderColor:[]}
-                            ]}
-                            source={imagen.path}
-                            resizeMode="contain"
-                        />}
-                        
+            /* You can only pick a card when the level was started and the game is not in pause.*/
+                        onPress={() => { if(userReady && !pause) handleCardClick(index)}}> 
+                {/* Mechanic for show cards again and give feedback: */}              
+                        {!pause ? (
+                            <Image
+                                style={[
+                                globalStyles.tinyLogo,
+                                globalStyles.card]}
+                                source={imagen.path}
+                                resizeMode="contain"
+                            />
+                        ) : (   
+                            (qcards[index].id === targetImage.id ? (
+                                <Image
+                                    style={[
+                                    globalStyles.tinyLogo,
+                                    globalStyles.card,
+                                    {borderColor:'green'} 
+                                    ]}
+                                    source={imagen.path}
+                                    resizeMode="contain"
+                                />
+                            ) : ( 
+                                <Image
+                                    style={[
+                                    globalStyles.tinyLogo,
+                                    globalStyles.card,
+                                    {borderColor:'red'} 
+                                    ]}
+                                    source={imagen.path}
+                                    resizeMode="contain"
+                                />   
+                            ))
+                        )}
                     </TouchableOpacity> 
                 ))}
-                
             </View>
-            <Animated.View style={{
-                opacity: animatedValue, 
-                transform: [
-                    {
-                    translateY: animatedValue.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [0, 10], // animar en el eje Y de 0 a 100
-                    }),
-                    },
-                ],
-                }}>
-                {!userReady && <Text style={globalStyles.text}>Cuando hayas memorizado las tarjetas pulsa "Estoy listo"</Text>}
+            {/* Animation for a short intruction */}
+            <Animated.View style={{ opacity: animatedValue, 
+                transform: [{ translateY: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, 10]}),
+                    },],
+                }}> {!userReady && <Text style={globalStyles.text}>Cuando hayas memorizado las tarjetas pulsa "Estoy listo"</Text>}
             </Animated.View>
+            {/* "Estoy Listo" Button: */}
             <View style={{ marginVertical: 10 }} /> 
             {!userReady  && <CustomButton title="Estoy listo" onPress={startNewLevel} width='30%' height={45}/>}
             
+            {/* feedback messagge: */}
             {pause && feedbackMessage !== '' && <Text style={globalStyles.text}>{feedbackMessage}</Text>}
+            {/* Asked Card: */}
             {userReady && targetImage && (
                 <View style={{marginTop: '0%'}}>
                     <Text style={globalStyles.text}>¿Dónde estaba esta tarjeta?</Text>
-                    <Image
-                            style={[
-                                {alignSelf: "center"},
-                                globalStyles.card,
-                            ]}
-                            source={targetImage.path}
-                            resizeMode="contain"
-                        />
+                    <Image style={[
+                        {alignSelf: "center"},
+                        globalStyles.card,]}
+                        source={targetImage.path}
+                        resizeMode="contain"
+                    />
                 </View> 
             )}
-            
-            
         </View>
     )
 }
